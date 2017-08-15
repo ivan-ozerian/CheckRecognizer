a832b6b21eadae8c718d514dac175865ab1cee7a
iozerian <iozerian@varteq.com>
2017-08-15 23:41:54 +0300

    Created config file witn bean declaration.

---
+++ src/main/java/com/ozerian/recognizer/config/SpringCoreConfig.java
@@ -0,0 +1,30 @@
+package com.ozerian.recognizer.config;
+
+import net.sourceforge.tess4j.ITessAPI;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class SpringCoreConfig {
+
+    @Value("${config.recognition.segmentation.mode}")
+    private int segmentationMode;
+
+    @Value("${config.recognition.language}")
+    private String recognitionLanguage;
+
+    @Bean
+    public ITesseract tesseract() {
+        ITesseract tesseract = new Tesseract();
+        tesseract.setOcrEngineMode(ITessAPI.TessOcrEngineMode.OEM_TESSERACT_ONLY);
+        tesseract.setPageSegMode(segmentationMode);
+        tesseract.setLanguage(recognitionLanguage);
+
+        return tesseract;
+    }
+}
