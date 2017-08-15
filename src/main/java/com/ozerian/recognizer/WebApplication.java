6b89bc3a90b5da2648b58fc7aca3ec79cff54674
iozerian <iozerian@varteq.com>
2017-08-15 23:43:05 +0300

    Transferred tesseract bean to config class.

--- src/main/java/com/ozerian/recognizer/WebApplication.java
+++ src/main/java/com/ozerian/recognizer/WebApplication.java
@@ -1,13 +1,9 @@
 package com.ozerian.recognizer;
 
-import net.sourceforge.tess4j.ITessAPI;
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
@@ -15,16 +11,6 @@ public class WebApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(WebApplication.class);
-    }
-
-    @Bean
-    public ITesseract tesseract() {
-        ITesseract tesseract = new Tesseract();
-        tesseract.setOcrEngineMode(ITessAPI.TessOcrEngineMode.OEM_TESSERACT_ONLY);
-        tesseract.setPageSegMode(3);
-        tesseract.setLanguage("ukr");
-
-        return tesseract;
     }
 
     public static void main(String[] args) {
